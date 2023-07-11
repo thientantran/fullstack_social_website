b93df7e34a159d31b33c98818acb01280547f151
thientantran <thientantm@gmail.com>
2023-07-11 14:56:26 +0700

    get friend list of an user

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -9,3 +9,21 @@ export const getUser = async(req, res) => {
     res.status(404).json({error: err.message})
   }
 }
+export const getUserFriends = async(req, res) => {
+  try {
+    const {id} = req.params;
+    const user = await User.findById(id);
+    // nhớ await lại
+    const friends = await Promise.all(
+      user['friends'].map((id) => User.findById(id))
+    )
+    const formattedFriends = friends.map(
+      ({ _id, firstName, lastName, occupation, location, picturePath }) => {
+        return { _id, firstName, lastName, occupation, location, picturePath };
+      }
+    );
+    res.status(200).json(formattedFriends)
+  } catch (err) {
+    res.status(400).json({error: err.message})
+  }
+}
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,7 +1,8 @@
 import express from "express";
-import { getUser } from "../controllers/user.js";
+import { getUser, getUserFriends } from "../controllers/user.js";
 import { verifyToken } from "../middleware/auth.js";
 const userRouter = express.Router();
 
 userRouter.get("/:id", verifyToken,getUser)
+userRouter.get("/:id/friends", verifyToken, getUserFriends);
 export default userRouter
