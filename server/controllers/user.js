49cc73dfb4dea26294bfe6fbb1bbf4427dc6ea99
thientantran <thientantm@gmail.com>
2023-07-11 15:29:28 +0700

    add or remove friend of an user

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -27,3 +27,36 @@ export const getUserFriends = async(req, res) => {
     res.status(400).json({error: err.message})
   }
 }
+
+export const addRemoveFriend = async(req, res) =>{
+  try {
+    const {id, friendId} = req.params
+    const user = await User.findById(id);
+    const friend = await User.findById(friendId);
+    if(user['friends'].includes(friendId)){
+      user['friends'] = user['friends'].filter((id) => id !== friendId)
+      friend['friends'] = user['friends'].filter((idInFriend) => idInFriend !== id)
+    }else{
+      user['friends'].push(friendId)
+      friend['friends'].push(id)
+    }
+    await user.save()
+    await friend.save()
+
+    const friends = await Promise.all(
+      user['friends'].map((id)=> {
+        const friendOfUser = User.findById(id)
+        if(friendOfUser !== null){
+          return friendOfUser
+        }
+      })
+    );
+    console.log(friends)
+    const formattedFriends = friends.map(({_id, firstName, lastName, occupation, location, picturePath})=>{
+      return {_id, firstName, lastName, occupation, location, picturePath}
+    })
+    res.status(200).json(formattedFriends)
+  } catch (err) {
+    res.status(404).json({error:err.message})
+  }
+}
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getUser, getUserFriends } from "../controllers/user.js";
+import { addRemoveFriend, getUser, getUserFriends } from "../controllers/user.js";
 import { verifyToken } from "../middleware/auth.js";
 const userRouter = express.Router();
 
 userRouter.get("/:id", verifyToken,getUser)
 userRouter.get("/:id/friends", verifyToken, getUserFriends);
+userRouter.patch("/:id/:friendId", verifyToken, addRemoveFriend);
 export default userRouter
