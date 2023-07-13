72249015b39af41b74710582998f3afe533086aa
thientantran <thientantm@gmail.com>
2023-07-13 13:52:46 +0700

    Layout for login page

--- client/src/scenes/LoginPage.jsx
+++ client/src/scenes/LoginPage.jsx
@@ -1,3 +1,32 @@
+import { Box, Typography, useMediaQuery, useTheme } from "@mui/material";
+
+import Form from "../components/Form";
+
 export default function LoginPage() {
-  return <div>LoginPage</div>;
+  const theme = useTheme();
+  const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
+  return (
+    <Box>
+      <Box
+        width="100%"
+        bgcolor={theme.palette.background.alt}
+        p="1rem 6%"
+        textAlign="center"
+      >
+        <Typography>Sociopedia</Typography>
+      </Box>
+      <Box
+        width={isNonMobileScreens ? "50%" : "93%"}
+        p="2rem"
+        m="2rem auto"
+        borderRadius="1.5rem"
+        bgcolor={theme.palette.background.alt}
+      >
+        <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
+          Welcome to Sociopedia, the social Media for Sociopaths
+        </Typography>
+        <Form />
+      </Box>
+    </Box>
+  );
 }
