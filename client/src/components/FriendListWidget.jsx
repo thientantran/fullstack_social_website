c5ac0f87c2112939ac21bc0ce22e9b776052d72f
thientantran <thientantm@gmail.com>
2023-07-15 15:35:37 +0700

    Layout Friend List

--- client/src/components/FriendListWidget.jsx
+++ client/src/components/FriendListWidget.jsx
@@ -1,5 +1,28 @@
+import { Stack, Typography, useTheme } from "@mui/material";
 import React from "react";
 
+import Friend from "./Friend";
+import WidgetWrapper from "./WidgetWrapper";
+
 export default function FriendListWidget() {
-  return <div>FriendListWidget</div>;
+  const theme = useTheme();
+  return (
+    <WidgetWrapper>
+      <Typography
+        color={theme.palette.neutral.dark}
+        variant="h5"
+        fontWeight="500"
+        sx={{ mb: "1.5rem" }}
+      >
+        Friend List
+      </Typography>
+      <Stack direction="column" gap="1.5rem">
+        {Array(5)
+          .fill(0)
+          .map((index) => (
+            <Friend key={index} />
+          ))}
+      </Stack>
+    </WidgetWrapper>
+  );
 }
