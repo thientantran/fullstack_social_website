962ee05c1d7c00f17acc9d20b7e3795c102a655d
thientantran <thientantm@gmail.com>
2023-07-15 15:57:34 +0700

    Layout Profile Page

--- client/src/scenes/ProfilePage.jsx
+++ client/src/scenes/ProfilePage.jsx
@@ -1,3 +1,36 @@
+import { Box, useMediaQuery } from "@mui/material";
+
+import FriendListWidget from "../components/FriendListWidget";
+import MyPostWidget from "../components/MyPostWidget";
+import PostsWidget from "../components/PostsWidget";
+import UserWidget from "../components/UserWidget";
+import Navbar from "./Navbar";
 export default function ProfilePage() {
-  return <div>ProfilePage</div>;
+  const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
+  return (
+    <Box>
+      <Navbar />
+      <Box
+        width="100%"
+        padding="2rem 6%"
+        display={isNonMobileScreens ? "flex" : "block"}
+        gap="2rem"
+        justifyContent="center"
+      >
+        <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
+          <UserWidget />
+          <Box m="2rem 0" />
+          <FriendListWidget />
+        </Box>
+        <Box
+          flexBasis={isNonMobileScreens ? "42%" : undefined}
+          mt={isNonMobileScreens ? undefined : "2rem"}
+        >
+          <MyPostWidget />
+          <Box m="2rem 0" />
+          <PostsWidget />
+        </Box>
+      </Box>
+    </Box>
+  );
 }
