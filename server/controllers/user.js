db46361692b9d7bf8c5c4f3842cd973b00a738f3
thientantran <thientantm@gmail.com>
2023-07-16 14:34:24 +0700

    Fixed Server

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,10 +1,11 @@
 import User from "../models/User.js";
 
 export const getUser = async(req, res) => {
+  console.log(req)
   try {
     const {id} = req.params;
     const user = await User.findById(id);
-    res.status(200).json({...a})
+    res.status(200).json(user)
   } catch (err) {
     res.status(404).json({error: err.message})
   }
