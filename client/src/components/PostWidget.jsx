46544c18233ac320236e182823cbefa6d9ce305c
thientantran <thientantm@gmail.com>
2023-07-16 16:44:49 +0700

    Get AllPosts and binding

--- client/src/components/PostWidget.jsx
+++ client/src/components/PostWidget.jsx
@@ -7,31 +7,31 @@ import {
 } from "@mui/icons-material";
 import { Box, Divider, IconButton, Typography, useTheme } from "@mui/material";
 import React, { useState } from "react";
+import { useSelector } from "react-redux";
 
 import FlexBetween from "./FlexBetween";
 import Friend from "./Friend";
 import WidgetWrapper from "./WidgetWrapper";
 
-export default function PostWidget({
-  picturePath = "https://townsquare.media/site/252/files/2021/08/attachment-blackpink-jisoo-blonde-e1628094363838.jpg",
-}) {
+export default function PostWidget({ post }) {
   const [isComment, setIsComment] = useState(false);
   const theme = useTheme();
-  const isLiked = true;
-  const likeCounts = 1000;
+  const loggedUserId = useSelector((state) => state.user._id);
+  const isLiked = Boolean(post.likes[loggedUserId]);
+  const likeCounts = Object.keys(post.likes).length;
   return (
     <WidgetWrapper m="2rem 0">
       <Friend />
-      <Typography color={theme.palette.neutral.main} sx={{ mt: "1rem" }}>
-        Posts Description
+      <Typography color={theme.palette.neutral.main} sx={{ m: "1rem 0" }}>
+        {post.description}
       </Typography>
-      {picturePath && (
+      {post.picturePath && (
         <img
           width="100%"
           height="auto"
           alt="post"
           style={{ borderRadius: "0.75rem", marginTop: ").75rem" }}
-          src={picturePath}
+          src={`http://localhost:6060/assets/${post.picturePath}`}
         />
       )}
 
@@ -54,7 +54,7 @@ export default function PostWidget({
             <IconButton onClick={() => setIsComment(!isComment)}>
               <ChatBubbleOutlineOutlined />
             </IconButton>
-            <Typography>Số lượng comment 1000</Typography>
+            <Typography>{post.comments.length}</Typography>
           </FlexBetween>
         </FlexBetween>
 
@@ -67,20 +67,19 @@ export default function PostWidget({
       {/* Write COMMENT */}
       {isComment && (
         <Box>
-          {Array(5)
-            .fill(0)
-            .map((_, index) => (
-              <Box key={index}>
-                <Divider />
-                <Typography
-                  sx={{
-                    color: theme.palette.primary.main,
-                    m: "0.5rem 0",
-                    pl: "1rem",
-                  }}
-                >{`comment ${index}`}</Typography>
-              </Box>
-            ))}
+          {/* chỉ thay đổi array thôi */}
+          {post.comments.map((comment, index) => (
+            <Box key={index}>
+              <Divider />
+              <Typography
+                sx={{
+                  color: theme.palette.primary.main,
+                  m: "0.5rem 0",
+                  pl: "1rem",
+                }}
+              >{`${comment}`}</Typography>
+            </Box>
+          ))}
           <Divider />
         </Box>
       )}
--- client/src/components/PostsWidget.jsx
+++ client/src/components/PostsWidget.jsx
@@ -1,12 +1,31 @@
-import React from "react";
+import { Stack } from "@mui/material";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
+import { setPosts } from "../Store";
 import PostWidget from "./PostWidget";
 
 export default function PostsWidget() {
+  const dispatch = useDispatch();
+  const posts = useSelector((state) => state.posts);
+  const token = useSelector((state) => state.token);
+
+  const getPosts = async () => {
+    const response = await fetch("http://localhost:6060/post/", {
+      method: "GET",
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    const data = await response.json();
+    dispatch(setPosts({ posts: data }));
+  };
+  useEffect(() => {
+    getPosts();
+  }, []);
   return (
-    <>
-      <PostWidget />
-      <PostWidget />
-    </>
+    <Stack direction="column-reverse">
+      {posts.map((post) => (
+        <PostWidget key={post._id} post={post} />
+      ))}
+    </Stack>
   );
 }
